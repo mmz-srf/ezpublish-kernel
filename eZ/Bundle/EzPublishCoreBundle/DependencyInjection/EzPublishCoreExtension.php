5112618d5e27f4682f17f4272981b51e4d81a1b4
Vidar Langseid <vl@ez.no>
2017-11-02 13:33:44 +0100

    Fixed so that it possible to define new purge_type in other bundles

--- eZ/Bundle/EzPublishCoreBundle/DependencyInjection/EzPublishCoreExtension.php
+++ eZ/Bundle/EzPublishCoreBundle/DependencyInjection/EzPublishCoreExtension.php
@@ -374,6 +374,7 @@ class EzPublishCoreExtension extends Extension implements PrependExtensionInterf
     {
         $loader->load('cache.yml');
 
+        $purgeService = null;
         if (isset($config['http_cache']['purge_type'])) {
             switch ($config['http_cache']['purge_type']) {
                 case 'local':
@@ -382,15 +383,11 @@ class EzPublishCoreExtension extends Extension implements PrependExtensionInterf
                 case 'http':
                     $purgeService = 'ezpublish.http_cache.purge_client.fos';
                     break;
-                default:
-                    if (!$container->has($config['http_cache']['purge_type'])) {
-                        throw new \InvalidArgumentException("Invalid ezpublish.http_cache.purge_type. Can be 'single', 'multiple' or a valid service identifier implementing PurgeClientInterface.");
-                    }
-
-                    $purgeService = $config['http_cache']['purge_type'];
             }
 
-            $container->setAlias('ezpublish.http_cache.purge_client', $purgeService);
+            if ($purgeService !== null) {
+                $container->setAlias('ezpublish.http_cache.purge_client', $purgeService);
+            }
         }
     }
 
